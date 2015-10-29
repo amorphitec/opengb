f8039e792f29c0d4cd84cad9ffc48a0fd22b7d95
amorphic <jstewart101@gmail.com>
2015-10-30 01:56:55 +1100

    Add head movement to test web interface.

--- opengb/static/js/opengb_gui.js
+++ opengb/static/js/opengb_gui.js
@@ -5,26 +5,29 @@ function switchContent(content) {
 } 
 
 function parseMessage(message) {
-  console.log(message.data)
   data = JSON.parse(message.data)
-  switch(data.event) {
-    case 'temp':
-      $("#bed-temp-current").text(data.params.bed_current);
-      $("#bed-temp-target").text(data.params.bed_target);
-      $("#nozzle1-temp-current").text(data.params.nozzle1_current);
-      $("#nozzle1-temp-target").text(data.params.nozzle1_target);
-      $("#nozzle2-temp-current").text(data.params.nozzle2_current);
-      $("#nozzle2-temp-target").text(data.params.nozzle2_target);
-      break;
-    case 'state':
-      $("#state").text(data.new);
-      break;
-    default:
-      console.log('Could not parse message: ' + data);
+  if ("event" in data) {
+    switch(data.event) {
+      case 'temp':
+        $("#bed-temp-current").text(data.params.bed_current);
+        $("#bed-temp-target").text(data.params.bed_target);
+        $("#nozzle1-temp-current").text(data.params.nozzle1_current);
+        $("#nozzle1-temp-target").text(data.params.nozzle1_target);
+        $("#nozzle2-temp-current").text(data.params.nozzle2_current);
+        $("#nozzle2-temp-target").text(data.params.nozzle2_target);
+        break;
+      case 'state':
+        $("#state").text(data.new);
+        break;
+      default:
+        console.log('Could not parse message: ' + data);
+    }
+  } else {
+    console.log(data.result);
   }
 }
 
-function sendMessage() {
+function setTemp() {
   var message = {
     'jsonrpc': '2.0',
     'id':       1,
@@ -34,6 +37,30 @@ function sendMessage() {
       'nozzle1':  206,
       'nozzle2':  203,
     }
+  };
+  socket.send(JSON.stringify(message)); 
+}
+
+function moveHead(x, y, z) {
+  var message = {
+    'jsonrpc': '2.0',
+    'id':       1,
+    'method':   'move_head',
+    'params': {
+      'x':  x,
+      'y':  y,
+      'z':  z,
+    }
+  };
+  socket.send(JSON.stringify(message)); 
+}
+
+function getCounter() {
+  var message = {
+    'jsonrpc': '2.0',
+    'id':       2,
+    'method':  'get_counters',
+    'params': {}
   };
   socket.send(JSON.stringify(message)); 
 }
