61d11a7db0d17516b415b5362074426d6bd80470
amorphic <jstewart101@gmail.com>
2015-11-22 00:44:12 +1100

    Add gcode file output dir to config

--- opengb/config.py
+++ opengb/config.py
@@ -12,6 +12,7 @@ CONFIG_FILE = "/etc/opengb/opengb.conf"
 define('http_port', default=80, help='Webserver http listen port')
 define('debug', default=False, help='Run in debug mode')
 define('db_file', default='/var/opengb/opengb.db', help='SQLite database')
+define('gcode_dir', default='/var/opengb/gcode/', help='GCode directory')
 define('printer', default='Dummy', help='Printer type')
 define('baud_rate', default=115200, help='Printer baud rate')
 define('serial_port', default='/dev/ttyACM0', help='Printer serial port')
