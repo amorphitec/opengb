9c04a41aece8a6a10319036756d93fd384276e3e
amorphic <jstewart101@gmail.com>
2016-01-25 11:55:07 +1100

    Update dummy printer for move_head_relative

--- opengb/printer/dummy.py
+++ opengb/printer/dummy.py
@@ -78,7 +78,7 @@ class Dummy(IPrinter):
         if nozzle2:
             self._temp_nozzle2 = nozzle2
 
-    def move_head(self, x=0, y=0, z=0):
+    def move_head_relative(self, x=0, y=0, z=0):
         self._callbacks.log(logging.DEBUG, 'Moving print head: x|{0}, '
                                            'y|{1}, z|{2}'.format(x, y, z))
 
