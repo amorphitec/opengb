4e531e736da736eb6559bcbb192c204594569c54
amorphic <jstewart101@gmail.com>
2016-02-17 10:34:00 +1100

    Update config defaults

--- opengb/config.py
+++ opengb/config.py
@@ -9,11 +9,12 @@ from tornado.options import define
 CONFIG_FILE = "/etc/opengb/opengb.conf"
 
 
-define('http_port', default=80, help='Webserver http listen port')
+define('http_port', default=8000, help='Webserver http listen port')
 define('debug', default=False, help='Run in debug mode')
 define('db_file', default='/var/opengb/db/opengb.db', help='SQLite database')
 define('gcode_dir', default='/var/opengb/gcode/', help='GCode directory')
 define('printer', default='Dummy', help='Printer type')
 define('baud_rate', default=115200, help='Printer baud rate')
-define('serial_port', default='/dev/ttyACM0', help='Printer serial port')
+define('serial_port', default=None,
+       help='Printer serial port (use "None" for auto)')
 define('frontend', default='opengb', help='Frontend (use "None" to disable)')
