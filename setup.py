f2a7878f929406e1e34bdd0717d79926b268f9b2
James Stewart <jstewart101@gmail.com>
2016-07-17 23:52:57 +1000

    Bring version up to date

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='opengb',
-    version='0.22.0',
+    version='0.22.3',
     author='re:3D',
     author_email='support@re3D.org',
     description='OpenGB printer control.',
